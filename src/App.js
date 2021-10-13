5869e0ac923693be9b51cdd5962695bd3fe6218f
Deniz ÇAY <idenizcay@gmail.com>
2021-10-13 13:30:45 +0300

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {tasks: [
+      { task: "Go shopping", isDone: false},
+      { task: "Do homework", isDone: true}
+    ]};
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>My Todos</h1>
+        <ol className="todos-list">
+          {this.state.tasks.map((value, index) => 
+          <li key={index}>
+            {value.task}
+            <input type="checkbox" checked={value.isDone} onChange={(e) => this.updateTodoStatus(e, index)} />
+          </li>)}
+        </ol>
+      </div>
+    );
+  }
+
+  updateTodoStatus(e, index){
+    const newTodos = [...this.state.tasks]; // mevcut dizinin kopyası
+    newTodos[index].isDone = e.target.checked;
+    this.setState({task: newTodos});
+  }
 }
 
 export default App;
+
